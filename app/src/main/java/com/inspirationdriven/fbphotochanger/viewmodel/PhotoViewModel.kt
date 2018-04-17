f7b5d324b5d5f87ca9e3d3bb33bb44b5fd8a8459
Sramische <sramische@gmail.com>
2018-04-17 17:33:32 +0300

    Storing profile pic url in shared prefs

--- app/src/main/java/com/inspirationdriven/fbphotochanger/viewmodel/PhotoViewModel.kt
+++ app/src/main/java/com/inspirationdriven/fbphotochanger/viewmodel/PhotoViewModel.kt
@@ -1,7 +1,9 @@
 package com.inspirationdriven.fbphotochanger.viewmodel
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import android.content.Context
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.inspirationdriven.fbphotochanger.getProfilePicture
@@ -9,20 +11,40 @@ import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class PhotoViewModel : ViewModel() {
-
+class PhotoViewModel(app: Application) : AndroidViewModel(app) {
     val profilePicUrl = MutableLiveData<String>()
     val loading = MutableLiveData<Boolean>()
     val overridenProfilePic = MutableLiveData<String>()
 
-    fun fetchProfilePicture(width: Int) {
+    init {
+        val p = getApplication<Application>().getSharedPreferences(PhotoViewModel::class.java.simpleName, Context.MODE_PRIVATE)
+                .getString(PREF_KEY_PROFILE_URL, null)
+        profilePicUrl.value = p
+    }
+
+    fun fetchProfilePicture(width: Int, force: Boolean = false) {
+        if (profilePicUrl.value != null && !force) return
         getProfilePicture(width).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ profilePicUrl.value = it }, { e -> e.printStackTrace() })
     }
 
-    fun setProfilePic(url: String){
+    fun setProfilePic(url: String) {
         profilePicUrl.value = url
+    }
+
+    override fun onCleared() {
+        profilePicUrl.value?.let {
+            getApplication<Application>().getSharedPreferences(PhotoViewModel::class.java.simpleName, Context.MODE_PRIVATE)
+                    .edit()
+                    .putString(PREF_KEY_PROFILE_URL, it)
+                    .apply()
+        }
+        super.onCleared()
+    }
+
+    companion object {
+        const val PREF_KEY_PROFILE_URL = "profile-url"
     }
 }
 
